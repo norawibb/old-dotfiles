761245692ec424333c7c34860c274763e9d7b2c2
nonwibb <nonwibb@gmail.com>
2021-09-15 09:19:36 -0400

    Add wallpaper stream script

---
+++ wallstream.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+killall xwinwrap
+
+sleep 0.3
+
+streamlink https://www.youtube.com/watch?v=DWcJFNfaw9c best --player="xwinwrap -ov -g 1920x1080+0+0 -- mpv -wid WID "$1" --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-keepaspect --no-input-default-bindings --hwdec" &
+streamlink https://www.youtube.com/watch?v=5qap5aO4i9A best --player="xwinwrap -ov -g 1920x1080+1920+0 -- mpv -wid WID "$1" --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --panscan=1.0 --no-keepaspect --no-input-default-bindings --hwdec"
